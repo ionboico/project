37a3ae50bdb3633ef17b7153c47e6ca976729f3b
iboico-arch <ion.boico97@gmail.com>
2020-09-09 15:41:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use sctrict";
+
+/*let number = 4.6;
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+// console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[4]);*/
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('What is your firstname?', '');
+// answers[1] = prompt('What is your lastname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Hello, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(2*4 === 8);
+
+// const isChecked = false,
+//       isClosed = false;
+
+// // console.log(isChecked && isClosed);
+// console.log(isChecked || isClosed);
+
